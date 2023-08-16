2f0b0d445df71e2481cbb4fe65f9c6fbadce494c
Vinod <vinod.awar@gmail.com>
2023-08-16 15:29:53 +0530

    Accessing JPA Data with REST

---
+++ gs-accessing-data-rest/src/main/java/com/example/jparest/GsAccessingDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jparest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAccessingDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAccessingDataRestApplication.class, args);
+	}
+
+}
